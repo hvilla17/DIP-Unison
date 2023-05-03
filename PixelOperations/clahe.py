1faec2ff7a9a18ffc4d867a0905bf0dcec81305c
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-05-02 17:56:33 -0700

    Add files via upload

---
+++ PixelOperations/clahe.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+
+image = cv.imread('tsukuba_gray.png', cv.IMREAD_GRAYSCALE)
+assert image is not None, "file could not be read, check with os.path.exists()"
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+image_clahe = clahe.apply(image)
+result = np.hstack((image, image_clahe))  # stacking images side-by-side
+cv.imwrite('tsukuba_clahe.png', result)
