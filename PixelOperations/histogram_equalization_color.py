5ace776bacafcf5ca294979230eb70390345c3fe
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-05-14 10:17:23 -0700

    Add files via upload

---
+++ PixelOperations/histogram_equalization_color.py
@@ -0,0 +1,30 @@
+# File: histogram_equalization_color.py
+
+import cv2 as cv
+import numpy as np
+
+image = cv.imread('9.bmp')
+
+# equalize each channel
+b, g, r = cv.split(image)
+equ_b = cv.equalizeHist(b)
+equ_g = cv.equalizeHist(g)
+equ_r = cv.equalizeHist(r)
+image_bgr = cv.merge((equ_b, equ_g, equ_r))
+
+result = np.hstack((image, image_bgr))
+
+cv.imwrite('9-equalized-bgr.bmp', result)
+
+# convert the BGR image to YUV format
+image_yuv = cv.cvtColor(image, cv.COLOR_BGR2YUV)
+
+# equalize the histogram of the Y channel
+image_yuv[:, :, 0] = cv.equalizeHist(image_yuv[:, :, 0])
+
+# convert the YUV image back to BGR format
+output = cv.cvtColor(image_yuv, cv.COLOR_YUV2BGR)
+
+result = np.hstack((image, output))
+
+cv.imwrite('9-equalized-yuv.bmp', result)
