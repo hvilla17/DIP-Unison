433dbe1bd486fa1af0837213111f414d453f7083
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-06-01 07:45:47 -0700

    Delete Introduction directory

--- Introduction/display_image.py
+++
@@ -1,7 +0,0 @@
-# File: display_image.py
-import cv2 as cv
-
-image = cv.imread('MyPic.png')
-cv.imshow('My image', image)
-cv.waitKey()
-cv.destroyAllWindows()		# destructor de ventanas
--- Introduction/display_image_matplot.py
+++
@@ -1,7 +0,0 @@
-# File: display_image_matplot.py
-import cv2 as cv
-import matplotlib.pyplot as plot
-
-image = cv.imread('MyPic.png')
-plot.imshow(image[:, :, ::-1])  # BGR to RGB
-plot.show()
