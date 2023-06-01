1d718db2e30a2a04a7ecd61875d2211d0cf3eeb9
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-06-01 12:01:23 -0700

    Add files via upload

--- image_descriptors/corner.py
+++ image_descriptors/corner.py
@@ -1,8 +1,15 @@
 import cv2 as cv
 
-img = cv.imread('../images/chess_board.png')
+img = cv.imread('../images/chessboard.png')
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 dst = cv.cornerHarris(gray, 2, 23, 0.04)
+
+# result is dilated for marking the corners, not important
+dst = cv.dilate(dst, None)
+
+# threshold for an optimal value, it may vary depending on the image
 img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+cv.imshow('output', dst)
 cv.imshow('corners', img)
 cv.waitKey()
