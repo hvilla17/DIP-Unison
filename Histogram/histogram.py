ffb4356b777ca6c90c5d32824aa5b2221dff7a5e
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-06-01 07:45:19 -0700

    Delete Histogram directory

--- Histogram/histogram.py
+++
@@ -1,7 +0,0 @@
-# File: histogram.py
-import cv2 as cv
-
-image = cv.imread('MyPic.png', cv.IMREAD_GRAYSCALE)
-assert image is not None, "file could not be read, check with os.path.exists()"
-hist = cv.calcHist([image], [0], None, [256], [0, 256])
-# hist is a 256x1 array
