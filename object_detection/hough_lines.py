0c289d3e2232af6698b865bed50a6047c755638d
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2023-09-06 10:52:30 -0700

    Add files via upload

--- object_detection/hough_lines.py
+++ object_detection/hough_lines.py
@@ -3,7 +3,7 @@ import cv2 as cv
 import numpy as np
 import math
 
-image = cv.imread('../images/building.jpg')
+image = cv.imread('../images/sudoku.png')
 image_gray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
 image_edges = cv.Canny(image_gray, threshold1=50, threshold2=200, apertureSize=3)
 
@@ -24,7 +24,7 @@ if lines is not None:
         y0 = b * rho
         pt1 = (int(x0 + 1000 * (-b)), int(y0 + 1000 * a))
         pt2 = (int(x0 - 1000 * (-b)), int(y0 - 1000 * a))
-        cv.line(image_hough_std, pt1, pt2, (0, 0, 255), 3, cv.LINE_AA)
+        cv.line(image_hough_std, pt1, pt2, color=(0, 0, 255), thickness=3, lineType=cv.LINE_AA)
 
 # probabilistic Hough
 linesP = cv.HoughLinesP(image_edges, rho=1, theta=np.pi / 180, threshold=80, minLineLength=30, maxLineGap=10)
@@ -32,7 +32,10 @@ linesP = cv.HoughLinesP(image_edges, rho=1, theta=np.pi / 180, threshold=80, min
 if linesP is not None:
     for i in range(0, len(linesP)):
         line = linesP[i][0]
-        cv.line(image_hough_p, (line[0], line[1]), (line[2], line[3]), (0, 0, 255), 3, cv.LINE_AA)
+        cv.line(image_hough_p,
+                pt1=(int(line[0]), int(line[1])),
+                pt2=(int(line[2]), int(line[3])),
+                color=(0, 0, 255), thickness=3, lineType=cv.LINE_AA)
 
-cv.imwrite('building_std.jpg', image_hough_std)
-cv.imwrite('building_p.jpg', image_hough_p)
+cv.imwrite('hough_std.jpg', image_hough_std)
+cv.imwrite('hough_p.jpg', image_hough_p)
