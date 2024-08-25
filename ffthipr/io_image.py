93b654423bcd55e4278ec1884cbfb53608516c2f
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2024-08-25 10:18:39 -0700

    Delete ffthipr directory

--- ffthipr/io_image.py
+++
@@ -1,9 +0,0 @@
-# https://courses.engr.illinois.edu/cs445/fa2023/
-
-import cv2 as cv
-import numpy as np
-
-
-def read_image(image_path):
-    intensity_image = cv.imread(image_path, cv.IMREAD_GRAYSCALE)
-    return intensity_image.astype(np.float32) / 255
