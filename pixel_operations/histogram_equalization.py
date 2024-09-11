bee5c8ef5ddd8a3439dff0e66f6bfe97843d9ebc
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2024-09-11 12:50:31 -0700

    Add files via upload

--- pixel_operations/histogram_equalization.py
+++ pixel_operations/histogram_equalization.py
@@ -2,9 +2,18 @@
 
 import cv2 as cv
 import numpy as np
+from matplotlib import pyplot as plot
 
 image = cv.imread('../images/Hawkes_Bay.jpg', cv.IMREAD_GRAYSCALE)
 assert image is not None, "file could not be read, check with os.path.exists()"
 image_equ = cv.equalizeHist(image)
 result = np.hstack((image, image_equ))  # stacking images side-by-side
 cv.imwrite('Hawkes_Bay_Equ.png', result)
+
+plot.figure(1)
+plot.title("Histograma original")
+plot.hist(image.ravel(), 256, (0, 256))
+plot.figure(2)
+plot.title("Histograma ecualizado")
+plot.hist(image_equ.ravel(), 256, (0, 256))
+plot.show()
