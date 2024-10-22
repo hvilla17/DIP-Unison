22e74ae90cbf126d627d72b68a27262f17f46a54
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2024-10-21 21:41:11 -0700

    Delete image_feature directory

--- image_feature/print_keypoint.py
+++
@@ -1,6 +0,0 @@
-# File: print_keypoint.py
-
-# prints a keypoint
-def print_keypoint(kp):
-    print(f'angle: {kp.angle}, class_id: {kp.class_id}, octave: {kp.octave}, \
-pt: {kp.pt}, response: {kp.response}, size: {kp.size}')
