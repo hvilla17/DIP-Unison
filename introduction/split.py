27dbf0cb114336f17c6b039dc4bdd114e3c6efef
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2025-08-30 21:26:07 -0700

    Add files via upload

--- introduction/split.py
+++ introduction/split.py
@@ -1,5 +1,6 @@
 # File: split.py
 import cv2 as cv
+from matplotlib import pyplot as plt
 
 image = cv.imread('../images/MyPic.png')
 
@@ -11,3 +12,15 @@ red = image[:, :, 2]
 cv.imwrite('MyPicBlue.jpg', blue)
 cv.imwrite('MyPicGreen.jpg', green)
 cv.imwrite('MyPicRed.jpg', red)
+
+# Display the RGB channels
+fig, ax = plt.subplots(1, 3, figsize=(12, 7))
+ax[0].imshow(red, cmap='Reds')
+ax[0].set_title('Red')
+ax[1].imshow(green, cmap='Greens')
+ax[1].set_title('Green')
+ax[2].imshow(blue, cmap='Blues')
+ax[2].set_title('Blue')
+fig.tight_layout()
+
+plt.show()
