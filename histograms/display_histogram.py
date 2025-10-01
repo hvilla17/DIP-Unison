90d8c8a544b530a94fae646c0e009bf7912546b4
hvilla17 <95720386+hvilla17@users.noreply.github.com>
2025-09-30 19:52:27 -0700

    Update display_histogram.py

--- histograms/display_histogram.py
+++ histograms/display_histogram.py
@@ -1,8 +1,8 @@
 # File: display_histogram.py
 import cv2 as cv
-from matplotlib import pyplot as plot
+from matplotlib import pyplot as plt
 
 image = cv.imread('../images/MyPic.png', cv.IMREAD_GRAYSCALE)
-assert image is not None, "file could not be read, check with os.path.exists()"
-plot.hist(image.ravel(), 256, [0, 256])
-plot.show()
+
+plt.hist(image.ravel(), 256, [0, 256])
+plt.show()
